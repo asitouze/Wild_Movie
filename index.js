b2aa665912ec0922a9ac54b30f72df3bbdc8cfd6
asitouze <aurelien.sitouze@gmail.com>
2023-11-16 15:17:57 +0100

    first save

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Pour le premier slider
 const plus = document.querySelector(".display-right")
 const moins =document.querySelector(".display-left")
 
@@ -25,6 +25,47 @@ function showDivs(n) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "flex";  
+  x[slideIndex-1].style.display = "flex"; 
 }
+
+
+  setInterval("plusDivs(1)",5000);
+
+
+
+
+// Pour le 2ème slider
+const plus1 = document.querySelector(".display-right1")
+const moins1 =document.querySelector(".display-left1")
+
+let slideIndex1 = 1;
+showDivs1(slideIndex1);
+
+const plusDivs1 = (n1) => {
+  showDivs1(slideIndex1 += n1);
+}
+
+moins1.addEventListener("click",() => {
+  plusDivs1(-1)
+})
+
+plus1.addEventListener("click", () => {
+  plusDivs1(1)
+})
+
+function showDivs1(n1) {
+  let i1;
+  const x1 = document.getElementsByClassName("mySlides1");
+  if (n1 > x1.length) {slideIndex1 = 1}
+  if (n1 < 1) {slideIndex1 = x1.length}
+  for (i1 = 0; i1 < x1.length; i1++) {
+    x1[i1].style.display = "none";  
+  }
+  x1[slideIndex1-1].style.display = "flex";  
+}
+
+setInterval("plusDivs1(1)",5000);
+
+
+
 
