bc5e87d03faac32cd258c8ae6a49b31483f1673f
asitouze <aurelien.sitouze@gmail.com>
2023-11-22 11:43:26 +0100

    add 3 slider

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ setInterval("plusDivs1(1)", 5000);
 
 
 icons.addEventListener("click", () => {
-  nav.classList.toggle("active")
+  nav.classList.toggle("active");
 })
 
 
@@ -103,5 +103,47 @@ links.forEach((link) => {
   })
 })
 
+// Pour le 3ème slider
 
+// On cible les élements
+const plus2 = document.querySelector(".display-right2")
+const moins2 = document.querySelector(".display-left2")
+
+// Initialisation de la variable slideIndex à 1
+let slideIndex2 = 1;
+showDivs2(slideIndex2);
+
+// Appelle la fonction showDivs et qui met à jour l'index de l'image de si on recule ou on avance
+const plusDivs2 = (n2) => {
+  showDivs2(slideIndex2 += n2);
+}
+
+// Fait reculer l'index de l'image de 1
+moins2.addEventListener("click", () => {
+  plusDivs2(-1)
+})
+
+// Fait avancer l'index de l'image de 1
+plus2.addEventListener("click", () => {
+  plusDivs2(1)
+})
+
+// Fonction qui va cacher les images et montrer l'image qu'il faut selon des conditions tactiques et stratégique
+function showDivs2(n2) {
+  let i2;
+  const x2 = document.getElementsByClassName("mySlides2");
+  if (n2 > x2.length) {
+    slideIndex2 = 1
+  }
+  if (n2 < 1) {
+    slideIndex1 = x1.length
+  }
+  for (i2 = 0; i2 < x2.length; i2++) {
+    x2[i2].style.display = "none";
+  }
+  x2[slideIndex2 - 1].style.display = "flex";
+}
+
+// Fait ce que PlusDivs réalise toutes les 5 secondes
+setInterval("plusDivs2(1)", 5000);
 
